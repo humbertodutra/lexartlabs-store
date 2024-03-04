f9d0e64588c9be783dec3db654a5c85f3300f28a
humberto <humberto.dutra@hotmail.com>
2024-03-04 20:41:12 -0300

    checking if the email is already registred

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,16 +2,24 @@ const userService = require('../service/userService');
 
 const registerUser = async (req, res) => {
   try {
-    if (!req.body.email || !req.body.password) {
-      return res.status(400).json({ message: 'Email and password are required' });
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: 'Email and password are required.' });
     }
 
-    const user = await userService.createUser(req.body);
-    res.status(201).json(user);
+    const existingUser = await userService.findUserByEmail(email);
+    if (existingUser) {
+      return res.status(409).json({ message: 'User with this email already exists.' });
+    }
+
+    const newUser = await userService.createUser(req.body);
+    res.status(201).json(newUser);
   } catch (error) {
-    res.status(400).json({ message: 'Error registering user', error });
+    res.status(400).json({ message: 'Error registering user.', error: error.message });
   }
 };
+
 
 const loginUser = async (req, res) => {
   try {
