3c7875da0f178fb9cc70525445a5f5a753dbd7eb
humberto <humberto.dutra@hotmail.com>
2024-03-04 20:16:14 -0300

    fixing pg dialect issue

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+import pg from 'pg';
 
 module.exports = {
   development: {
@@ -6,7 +7,7 @@ module.exports = {
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DATABASE,
     host: process.env.POSTGRES_HOST,
-    dialect: "postgres",
+    dialect: pg,
     dialectOptions: {
       ssl: {
         require: true,
@@ -19,7 +20,7 @@ module.exports = {
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DATABASE,
     host: process.env.POSTGRES_HOST,
-    dialect: "postgres",
+    dialect: pg,
     dialectOptions: {
       ssl: {
         require: true,
@@ -32,7 +33,7 @@ module.exports = {
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DATABASE,
     host: process.env.POSTGRES_HOST,
-    dialect: "postgres",
+    dialect: pg,
     dialectOptions: {
       ssl: {
         require: true,
