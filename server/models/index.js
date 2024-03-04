139582228e1bf47a00453825d9ff932644a3dbf5
humberto <humberto.dutra@hotmail.com>
2024-03-04 20:34:26 -0300

    fixing pg issue

--- server/models/index.js
+++ server/models/index.js
@@ -8,12 +8,14 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
+const pg = require('pg');
+
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], config, {dialect: 'postgres', dialectModule: pg});
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, config, {dialect: 'postgres', dialectModule: pg});
 }
 
 fs
