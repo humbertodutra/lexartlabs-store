9e8e38920eb9d7c7d56caa5c0579527136db5894
humberto <humberto.dutra@hotmail.com>
2024-03-06 14:40:45 -0300

    adding the enviroment variable at the register form

--- client/src/components/SingupForm/SingupForm.js
+++ client/src/components/SingupForm/SingupForm.js
@@ -13,7 +13,7 @@ function SignupForm({ onSignupSuccess }) {
 		}
 
 		try {
-			const response = await fetch(`http://localhost:5000/api/users/register`, {
+			const response = await fetch(`${process.env.REACT_APP_API_ENDPOINT}/api/users/register`, {
 				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json'
